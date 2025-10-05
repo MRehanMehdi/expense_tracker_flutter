6da7277e2904cb8821ad3898e6865b177bd64e6e
MRehanMehdi <m.rehanmehdi700@gmail.com>
2025-10-05 20:35:22 +0500

    update main.dart and expense.dart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -4,7 +4,7 @@ class Expense {
   final int? id;
   final String category;
   final double amount;
-  final String date;
+  final DateTime date;
 
   Expense({
     this.id,
@@ -13,21 +13,25 @@ class Expense {
     required this.date,
   });
 
+  // Convert Expense object to Map for database insertion
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'category': category,
       'amount': amount,
-      'date': date,
+      'date': date.toIso8601String(), // store date as string in ISO format
     };
   }
 
+  // Create Expense object from Map fetched from database
   factory Expense.fromMap(Map<String, dynamic> map) {
     return Expense(
       id: map['id'],
       category: map['category'],
-      amount: map['amount'],
-      date: map['date'],
+      amount: map['amount'] is int
+          ? (map['amount'] as int).toDouble()
+          : map['amount'],
+      date: DateTime.parse(map['date']),
     );
   }
 
